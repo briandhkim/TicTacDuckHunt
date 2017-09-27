8d961c48cd90b966bd7933417e8622b9b7dd46f3
briandhkim <w1057216@gmail.com>
2017-09-27 11:12:42 -0700

    final notes on clickgamesquare function in tictacmain

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 function startGame(){
 	var dimension = $("input[name='grid']:checked").val();
 	var ticTacMain = new TicTacMain(dimension);
+	var duck = new DuckLayer();
 	ticTacMain.createBoard(dimension);
 }
 
