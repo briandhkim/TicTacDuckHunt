f6b51461aee3731ebd28235e26426abec9355ac1
briandhkim <w1057216@gmail.com>
2017-09-27 11:17:56 -0700

    object variables edit in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
+var ticTacMain = null;
+var duck = null;
+
 function startGame(){
 	var dimension = $("input[name='grid']:checked").val();
-	var ticTacMain = new TicTacMain(dimension);
-	var duck = new DuckLayer();
+	ticTacMain = new TicTacMain(dimension);
+	duck = new DuckLayer();
 	ticTacMain.createBoard(dimension);
 }
 
