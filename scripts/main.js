87a7f40d1e5b8bafb3736bd3df84c981d43efd85
briandhkim <w1057216@gmail.com>
2017-09-30 12:53:26 -0700

    cursor change in progress

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
 audioHandler is not instantiated before being used - added var aduiHandler=null below var gameStarted
 reset button had remove/addClass typo - fixed typo
 score was not reset to blank(or zero) when reset was pressed
-added 0 to html playerScore span
+added 0 to html playerScore span - a395d5b(github commit id thing)
+cursor confined to gameScreenMonitor
+winnerDisplayMessage area message added
 */
 var ticTacMain = null;
 var duckLayer = null;
@@ -21,8 +23,8 @@ function startGame(){
         duckLayer.duckAnimation();
 		audioHandler = new AudioHandler();	//audioHandler missing var| set var audioHandler to null up top (Brian)
 		ticTacMain.createBoard(dimension);
-		$('body').removeClass('cursorDefault');
-		$('body').addClass('cursorPlayer0');
+		$('.gameScreenMonitor').removeClass('cursorDefault');
+		$('.gameScreenMonitor').addClass('cursorPlayer0');
 		$('.player0Name').addClass('playerFocusHighlight');
 		$('.player0Area').bind('click',playerTurnStart);
 		gameStarted = true;
@@ -34,10 +36,11 @@ function resetGame(){
 	if(gameStarted){
 		gameStarted = false;
 		$('.gameSquare').remove();
-		$('body').removeClass('cursorPlayer0 cursorPlayer1').addClass('cursorDefault');
+		$('.gameScreenMonitor').removeClass('cursorPlayer0 cursorPlayer1').addClass('cursorDefault');
 		$('.player0Name, .player1Name').removeClass('playerFocusHighlight');
 		$('.player0Area, .player1Area').unbind('click',playerTurnStart);
 		$('.player0ScoreVal, .player1ScoreVal').text('0');
+		$('.winnerMessageDisplay').text('select options above and click start');
 		ticTacMain = null;
 		duckLayer = null;
 		audioHandler = null;
